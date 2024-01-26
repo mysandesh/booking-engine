36b60ef169629c0a2dd26e76599024e337ef489d
Sandesh Acharya <mysandesh@gmail.com>
2024-01-26 18:45:11 +0545

    Search added

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "../Container";
 import Logo from "./Logo";
+import Search from "./Search";
 
 const Navbar = () => {
   return (
@@ -22,6 +23,7 @@ const Navbar = () => {
             "
           >
             <Logo />
+            <Search />
           </div>
         </Container>
       </div>
