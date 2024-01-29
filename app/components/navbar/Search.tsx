687ae9c985290663c9378f1b469923292d1c0c68
Sandesh Acharya <mysandesh@gmail.com>
2024-01-29 14:32:57 +0545

    Search styles implemented

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,39 @@
+"use client";
+
 const Search = () => {
-  return <div></div>;
+  return (
+    <div
+      className="
+    border-[1px]
+    w-full
+    md:w-auto
+    py-2
+    rounded-full
+    shadow-sm
+    hover:shadow-mdtransition
+    cursor-pointer
+    "
+    >
+      <div
+        className="
+        flex
+        flex-row
+        items-center
+        justify-between
+        "
+      >
+        <div
+          className="
+            text-sm
+            font-semibold
+            px-6
+            "
+        >
+          Anywhere
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
