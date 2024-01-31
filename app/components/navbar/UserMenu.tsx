81100ab79a74b19aaff3455372b33a10a4dbd83e
Sandesh Acharya <mysandesh@gmail.com>
2024-01-31 22:10:34 +0545

    UserMenu added and stylized

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,7 +1,29 @@
 "use client";
 
 const UserMenu = () => {
-  return <div></div>;
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="
+            hidden
+            md:block
+            text-sm
+            font-semibold
+            py-3
+            px-4
+            rounded-full
+            hover:bg-neutral-100
+            transition
+            cursor-pointer
+            "
+        >
+          BookMe your home
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserMenu;
