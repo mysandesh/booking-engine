8b20c4ff87927f234eadb70b825eaad184c3d51b
Sandesh Acharya <mysandesh@gmail.com>
2024-02-01 17:47:21 +0545

    UserMenu formatting added

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -21,6 +21,24 @@ const UserMenu = () => {
         >
           BookMe your home
         </div>
+        <div
+          onClick={() => {}}
+          className="
+            p-4
+            md:py-1
+            md:px-2
+            border-[1px]
+            border-neutral-200
+            flex
+            flex-row
+            items-center
+            gap-3
+            rounded-full
+            cursor-pointer
+            hover:shadow-md
+            transition
+            "
+        ></div>
       </div>
     </div>
   );
