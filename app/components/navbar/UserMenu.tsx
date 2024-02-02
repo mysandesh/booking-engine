b35c6123f55574e29faf66f4509cdfe3d226cfd2
Sandesh Acharya <mysandesh@gmail.com>
2024-02-02 23:10:32 +0545

    MenuItem added on Navbar

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,15 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -25,7 +32,7 @@ const UserMenu = () => {
           BookMe your home
         </div>
         <div
-          onClick={() => {}}
+          onClick={(toggleOpen) => {}}
           className="
             p-4
             md:py-1
@@ -47,6 +54,28 @@ const UserMenu = () => {
           <Avatar />
         </div>
       </div>
+      {isOpen && (
+        <div
+          className="
+          absolute
+          rounded-xl
+          shadow-md
+          w-[40vw]
+          md:w-3/4
+          bg-white
+          overflow-hidden
+          right-0
+          top-12
+          text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem onClick={() => {}} label="Login" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
