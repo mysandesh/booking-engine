5873adcf942607c4a1bc1b6fa9a22012d9458257
Sandesh Acharya <mysandesh@gmail.com>
2024-02-28 21:40:57 +0545

    Secondary Action Button added

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalProps {
   actionLabel: string;
   disabled?: boolean;
   secondaryAction?: () => void;
-  secondaryLabel?: string;
+  secondaryActionLabel?: string;
 }
 
 const Modal: React.FC<ModalProps> = ({
@@ -27,7 +27,7 @@ const Modal: React.FC<ModalProps> = ({
   actionLabel,
   disabled,
   secondaryAction,
-  secondaryLabel,
+  secondaryActionLabel,
 }) => {
   const [showModal, setShowModal] = useState(isOpen);
 
@@ -163,6 +163,12 @@ const Modal: React.FC<ModalProps> = ({
                 w-full
                 "
                 >
+                  <Button
+                    outline
+                    disabled={disabled}
+                    label={secondaryActionLabel}
+                    onClick={handleSecondaryAction}
+                  />
                   <Button
                     disabled={disabled}
                     label={actionLabel}
