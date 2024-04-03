9caf8b6761d03e2f3231d738b2df955e95a1c440
Sandesh Acharya <mysandesh@gmail.com>
2024-04-03 12:41:10 +0545

    React Hook Forms Added

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
 const RegisterModal = () => {
   return <div></div>;
 };
